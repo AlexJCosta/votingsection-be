0e4bc03eb55785ed57da0e6f72bf0d35004cb66a
Alex <alexjcosta01@gmail.com>
2021-01-06 19:08:58 -0300

    Adding swagger documentation

---
+++ src/main/java/com/votingsection/VotingSectionApplication.java
@@ -0,0 +1,13 @@
+package com.votingsection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingSectionApplication.class, args);
+	}
+
+}
