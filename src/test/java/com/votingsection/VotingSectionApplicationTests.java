9dabfa6257d60bbe0490917f2ed25fe9c1340506
Alex <alexjcosta01@gmail.com>
2021-01-06 19:09:22 -0300

    Addin basic tests

---
+++ src/test/java/com/votingsection/VotingSectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.votingsection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
